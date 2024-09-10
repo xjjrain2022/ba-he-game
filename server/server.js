01072144a803a01dfb89a8ccdde3ed7352292b25
xiajj <xjjrain2015@126.com>
2024-09-10 21:58:02 +0800

    添加后台

---
+++ server/server.js
@@ -0,0 +1,151 @@
+import { WebSocketServer, WebSocket } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let isGameStarting = false;
+let leftTeamMembers = [];
+let rightTeamMembers = [];
+let records = [];
+let leftScore = 0;
+let rightScore = 0;
+const POWER_ADD_SCORE = 50
+const WIN_SCORE = 1000
+let timerId = null;
+
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(msg) {
+        console.log('received: %s', msg);
+        const { type, data } = JSON.parse(msg)
+        switch(type) {
+            case 'join-team':
+                joinTeamHandler(ws, data);
+                break;
+            case 'member-leave':
+                memberLeaveHandler(ws, data);
+                break;
+            case 'start-game':
+                startGameHandler(ws, data);
+                break;
+            case 'add-power':
+                addPowerHandler(ws, data);
+                break;
+            case 'fetch-history':
+                fetchHistoryHandler(ws, data);
+                break;
+            
+            default:
+                break;
+        }
+    });
+
+    //ws.send('something');
+    initConnectionHandler(ws);
+});
+
+console.log('服务已启动，ws地址: ws://localhost:8080')
+
+// 初始链接到服务
+function initConnectionHandler(ws) {
+    const payload = createPayload('init')
+    ws.send(payload)
+}
+function joinTeamHandler(ws, data) {
+    const { name, team } = data;
+    const members = team === 'left' ? leftTeamMembers : rightTeamMembers;
+    if (!members.includes(name)) {
+        members.push(name)
+    }
+    sendToAllMembers(createPayload('join-team-completed'))
+}
+function memberLeaveHandler(ws, data) {
+    const { name, team } = data;
+    if (name === '' && team === '') {
+        return
+    }
+    const members = team === 'left' ? leftTeamMembers : rightTeamMembers;
+    const idx = members.findIndex(item => item === name)
+    if (idx !== -1) {
+        members.splice(idx, 1)
+    }
+    // 如果一方没人了，另一方获胜游戏结束
+    if (members.length === 0) {
+        isGameStarting = false;
+        sendToAllMembers(createGameOverPayload('member-leave-completed', team === 'left' ? 'right' : 'left'))
+    }
+    sendToAllMembers(createPayload('member-leave-completed'))
+}
+function startGameHandler(ws, data) {
+    isGameStarting = true
+    leftScore = 0;
+    rightScore = 0;
+    sendToAllMembers(createPayload('start-game-completed'))
+}
+
+function addPowerHandler(ws, data) {
+    const { name, team } = data;
+    const members = team === 'left' ? leftScore : rightTeamMembers;
+    if (team === 'left') {
+        leftScore += POWER_ADD_SCORE;
+    } else {
+        rightScore += POWER_ADD_SCORE
+    }
+    if (leftScore - rightScore > WIN_SCORE) {
+        sendToAllMembers(createGameOverPayload('add-power-completed', 'left'))
+        return;
+    }
+    if (rightScore - leftScore > WIN_SCORE) {
+        sendToAllMembers(createGameOverPayload('add-power-completed', 'right'))
+        return;
+    }
+
+    sendToAllMembers(createPayload('add-power-completed'))
+}
+
+function fetchHistoryHandler(ws, data) {
+    const { name, team } = data;
+    const datas = records.filter(item => item.team === team && item.members.includes(name))
+    ws.send(JSON.stringify({
+        type: 'fetch-history-completed',
+        data: datas
+    }))
+}
+
+// 创建消息体
+function createPayload(type, team = '') {
+    const payload = {
+        type,
+        data: {
+            isGameStarting,
+            leftTeamMembers,
+            rightTeamMembers,
+            team,
+            leftScore,
+            rightScore,
+        }
+    }
+    return JSON.stringify(payload)
+}
+
+function createGameOverPayload(type, team = '') {
+    const record = {
+        members: team === 'left' ? leftTeamMembers : rightTeamMembers,
+        timestamp: Date.now(),
+        team,
+        score: team === 'left' ? leftScore : rightScore,
+    }
+    records.push(record)
+    isGameStarting = false;
+    return createPayload(type, team)
+}
+
+// 发送消息给所有人
+function sendToAllMembers(payload) {
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(payload);
+        }
+    })
+}
--- src/App.vue
+++ src/App.vue
@@ -12,13 +12,30 @@ const teamScore = ref({
   left: 0,
   right: 0,
 })
-const sliderValue = computed(() => teamScore.value.left + teamScore.value.right)
+
 const formDiabled = ref(false)
 const marks = reactive({
   0: '中点',
   [WIN_SCORE]: '右队胜',
   [-WIN_SCORE]: '左队胜'
 })
+const gameInfo = ref({
+  isGameStarting: false,
+  leftTeamMembers: [],
+  rightTeamMembers: [],
+  team: '',
+  leftScore: 0,
+  rightScore: 0,
+})
+const sliderValue = computed(() => {
+  const {leftScore, rightScore} = gameInfo.value
+  return rightScore - leftScore
+})
+const allowGameStart = computed(() => {
+  const {isGameStarting, leftTeamMembers, rightTeamMembers} = gameInfo.value
+  return isGameStarting === false && leftTeamMembers.length && rightTeamMembers.length
+})
+
 // 从本地读取用户
 const historyUser = localStorage.getItem('bahe_user')
 if (historyUser) {
@@ -27,19 +44,79 @@ if (historyUser) {
   form.team = info.team
   formDiabled.value = true
 }
+
+const historyRecords = ref(null)
+
+// 连接 websocket 服务器
+const ws = ref(null)
+ws.value = new WebSocket('ws://localhost:8080')
+
+ws.value.onopen = function(event) {
+  console.log('WebSocket connected: ', event);
+  if (formDiabled.value) {
+    joinTeam()
+  }
+};
+
+ws.value.onerror = function(error) {
+  console.error('WebSocket error: ', error);
+};
+
+ws.value.onmessage = function(event) {
+  console.log('WebSocket message received: ', event.data);
+  const {type, data} = JSON.parse(event.data)
+  switch (type) {
+    case 'init':
+      gameInfo.value = data
+      break
+    case 'fetch-history-completed':
+      fetchHistoryCompleted(data)
+      break;
+  
+    default:
+      gameInfo.value = data
+      break;
+  }
+  
+};
+
+ws.value.onclose = function() {
+  console.log('WebSocket disconnected');
+  ws.value.send(JSON.stringify({
+    type: 'member-leave',
+    data: form
+  }))
+};
+
 const onSubmit = () => {
   console.log('submit!')
   const user = JSON.stringify(form)
   localStorage.setItem('bahe_user', user)
   formDiabled.value = true
+  joinTeam()
+}
+const onStartGame = () => {
+  ws.value.send(JSON.stringify({
+    type: 'start-game',
+    data: null
+  }))
 }
 const addPower = team => {
-  if (team === 'left') {
-    teamScore.value.left -= POWER_ADD_SCORE
+  console.log('gameInfo: ', gameInfo.value)
+  if (!gameInfo.value.isGameStarting) {
+    return
   }
-  if (team === 'right') {
-    teamScore.value.right += POWER_ADD_SCORE
-  }
+  console.log('send...')
+  ws.value.send(JSON.stringify({
+    type: 'add-power',
+    data: form
+  }))
+}
+const fetchHistory = () => {
+  ws.value.send(JSON.stringify({
+    type: 'fetch-history',
+    data: form
+  }))
 }
 watch(sliderValue, () => {
   if (sliderValue.value > WIN_SCORE) {
@@ -49,6 +126,26 @@ watch(sliderValue, () => {
     alert('左队获胜')
   }
 })
+
+function joinTeam() {
+  ws.value.send(JSON.stringify({
+    type: 'join-team',
+    data: form
+  }))
+}
+function fetchHistoryCompleted(data) {
+  historyRecords.value = data.map(item => {
+    const { team, score, timestamp } = item
+    const date = new Date(timestamp)
+    return {
+      score,
+      team: team === 'left' ? '左队' : '右队',
+      time: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+    }
+  })
+}
+
+
 </script>
 
 <template>
@@ -57,9 +154,16 @@ watch(sliderValue, () => {
       <h1>拔河比赛</h1>
     </el-col>
   </el-row>
-  <el-row :gutter="20">
-    <el-col :span="12" :offset="6">
-      <el-form :model="form" label-width="auto" style="max-width: 600px" :disabled="formDiabled">
+  <el-divider />
+  
+
+  <el-card style="margin-top: 20px;">
+    <template #header>
+      <div class="card-header">
+        <span>用户信息</span>
+      </div>
+    </template>
+    <el-form :model="form" label-width="auto" style="max-width: 600px" :disabled="formDiabled">
         <el-form-item label="用户名">
           <el-input v-model="form.name" />
         </el-form-item>
@@ -71,25 +175,29 @@ watch(sliderValue, () => {
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">加入游戏</el-button>
-          <el-button>历史记录</el-button>
+          
         </el-form-item>
       </el-form>
-    </el-col>
-  </el-row>
-  
-  <header>
-    
-  </header>
-  <el-row>
-    <el-col :span="24">
-      <h1>比赛情况</h1>
-      <div v-if="formDiabled">{{form.name}}加入了{{form.team === 'left' ? '左队' : '右队'}}</div>
-    </el-col>
-  </el-row>
+  </el-card>
 
-  <main>
+  
+
+  <el-card style="margin-top: 20px;">
+    <template #header>
+      <div class="card-header">
+        <span>比赛区</span>
+        <div>
+          <el-button type="success" @click="onStartGame" :disabled="!allowGameStart">开始比赛</el-button>
+        </div>
+      </div>
+    </template>
     <div class="game">
-      <el-button type="primary" class="power-btn" circle @click="addPower('left')" v-if="formDiabled && form.team === 'left'">为左队加力</el-button>
+      <el-button type="primary" 
+        class="power-btn" circle 
+        @click="addPower('left')" 
+        v-if="formDiabled && form.team === 'left'"
+        :disabled="!gameInfo.isGameStarting"
+      >为左队加力</el-button>
       <el-button type="info" class="power-btn" circle disabled v-else>左队</el-button>
       <el-slider class="game-line" 
         v-model="sliderValue" 
@@ -101,10 +209,39 @@ watch(sliderValue, () => {
       />
       <el-button type="danger" class="power-btn" circle @click="addPower('right')"
         v-if="formDiabled && form.team === 'right'"
+        :disabled="!gameInfo.isGameStarting"
       >为右队加力</el-button>
       <el-button type="info" class="power-btn" circle disabled v-else>右队</el-button>
     </div>
-  </main>
+  </el-card>
+
+  <el-card style="margin-top: 20px;">
+    <template #header>
+      <div class="card-header">
+        <span>历史记录</span>
+        <el-button type="primary" @click="fetchHistory">获取历史记录</el-button>
+      </div>
+    </template>
+    <el-table :data="historyRecords" style="width: 100%" v-if="historyRecords && historyRecords.length">
+      <el-table-column prop="team" label="队伍" width="180" />
+      <el-table-column prop="score" label="团队分" width="180" />
+      <el-table-column prop="time" label="日期" />
+    </el-table>
+  </el-card>
+  
+  <!-- <div>
+    <h1>比赛区</h1>
+    <div v-if="formDiabled">{{form.name}}加入了{{form.team === 'left' ? '左队' : '右队'}}</div>
+    <div>
+      <el-button type="success" @click="onStartGame" :disabled="!allowGameStart">开始比赛</el-button>
+    </div>
+  </div>
+  <el-divider />
+
+  <main>
+    
+    
+  </main> -->
 </template>
 
 <style scoped>
@@ -123,5 +260,9 @@ watch(sliderValue, () => {
 }
 .game-slider {
   background: yellow
+}
+.card-header {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
